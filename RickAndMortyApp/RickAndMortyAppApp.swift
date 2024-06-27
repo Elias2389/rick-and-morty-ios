ec07a2fa15567483f05c7846e965c1bd0cc69e59
Elias2389 <andreselias2303@gmail.com>
2024-06-27 13:55:57 +0200

    Initial Commit

---
+++ RickAndMortyApp/RickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppApp.swift
+//  RickAndMortyApp
+//
+//  Created by Andres Rivas on 27/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RickAndMortyAppTests/RickAndMortyAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppTests.swift
+//  RickAndMortyAppTests
+//
+//  Created by Andres Rivas on 27/06/2024.
+//
+
+import Testing
+@testable import RickAndMortyApp
+
+struct RickAndMortyAppTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
